cfb9461dc10cf0f635015a44ee1a9264283e5b50
Mona Lisa <mekotigi@gmail.com>
2022-01-17 03:02:46 -0500

    upload video

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer")
-const { initialQuestion, departmentQuestion, roleQuestions, employeeQuestions, updateQuestions } = require(".//questions")
+const { initialQuestion, departmentQuestion, roleQuestions, employeeQuestions, updateQuestions } = require("./db/questions")
 const cTable = require("console.table")
 const mysql = require("mysql2")
 require('dotenv').config()
